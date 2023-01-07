ba650ef3e1ab78e0d6da11477c54a60023f3b3ef
TelitsynIvan <neleonzend@gmail.com>
2023-01-07 19:48:50 +0300

    password

--- test/NorthwindContext.cs
+++ test/NorthwindContext.cs
@@ -71,7 +71,7 @@ public partial class NorthwindContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=Northwind;Trusted_Connection=False; User Id = sa; Password = 1155954534vV; Encrypt=false;");
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=Northwind;Trusted_Connection=False; User Id = sa; Password = pASSword; Encrypt=false;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
